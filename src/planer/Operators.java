3536f6e49f0b035b7afe1957cb6bc433e2460e8a
domenleskovec <leskovec.domen@gmail.com>
2015-01-09 23:15:17 +0100

    myFinalVersion

--- src/planer/Operators.java
+++ src/planer/Operators.java
@@ -5,7 +5,7 @@
  */
 package planer;
 
-import java.util.List;
+//import java.util.List;
 
 /**
  *
@@ -13,26 +13,55 @@ import java.util.List;
  */
 public class Operators {
     public String name,value1,value2;
-    public List<State> Preconditions, Eliminate, Add;
+    //public List<State> Preconditions, Eliminate, Add;
     
-    public Operators(String NAME, String VALUE_1, String VALUE_2, List<State> PREC, List<State> ELI, List<State> ADD)
+    public Operators(String NAME, String VALUE_1, String VALUE_2)//, List<State> PREC, List<State> ELI, List<State> ADD)
     {
         name = NAME;
         value1 = VALUE_1;
         value2 = VALUE_2;
-        Preconditions = PREC;
-        Eliminate = ELI;
-        Add = ADD;
+        //Preconditions = PREC;
+        //Eliminate = ELI;
+        //Add = ADD;
     }
     
-    public Operators(String NAME, String VALUE_1, List<State> PREC, List<State> ELI, List<State> ADD)
+    public Operators(String NAME, String VALUE_1)//, List<State> PREC, List<State> ELI, List<State> ADD)
     {
         name = NAME;
         value1 = VALUE_1;
         value2 = null;
-        Preconditions = PREC;
-        Eliminate = ELI;
-        Add = ADD;
+        //Preconditions = PREC;
+        //Eliminate = ELI;
+        //Add = ADD;
+    }
+    
+    public boolean eq (Operators st)
+    {
+        if(name.equals(st.name))
+        {
+            if((value1==null)&&(st.value1==null))
+            {
+                return true;
+            }
+            else
+            {
+                if((name.equals(st.name))&&(value1.equals(st.value1)))
+                {
+                    if((value2==null)&&(st.value2==null))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if((name.equals(st.name))&&(value1.equals(st.value1))&&(value2.equals(st.value2)))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
     
     
